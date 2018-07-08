079bf9a739622fa6edd3d88d7ac4da385e1a3873
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-08 11:00:07 +0300

    update

--- __init__.py
+++ __init__.py
@@ -113,7 +113,7 @@ class QuestionAnsweringSkill(MycroftSkill):
             answers = response.split(',')
             answers_count = len(answers)
             for i in range(answers_count):
-                answer = answers[i].capitalize()
+                answer = answers[i].strip().capitalize()
                 phrase += answer + (
                     ' . or ' if i == answers_count - 2 and answers_count > 1 else ' . ')
                 if any(char.isdigit() for char in answer) or answer in ('Yes', 'No'):
